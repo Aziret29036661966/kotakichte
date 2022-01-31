fcbcedd5ed1dd419354870ea949f6b2e23bee590
User <rasulov_aziret@mail.ru>
2022-01-31 21:03:12 +0600

    impruve

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (Hero dad: CreateHeroes()
-             ) {
+        for (Hero dad : CreateHeroes()) {
             System.out.println(dad);
         }
     }
 
-    public static Hero[] CreateHeroes(){
-Hero hero = new Hero("Thor","Zeus","Daun");
-        Hero [] ss = new Hero[] {hero};
+    public static Hero[] CreateHeroes() {
+        Hero hero = new Hero("Thor", "Zeus", "Daun");
+        Hero[] ss = new Hero[]{hero};
         return ss;
     }
 }
